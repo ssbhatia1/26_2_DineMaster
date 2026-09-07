53eca02e479357cc4a1961aaa7799cad17a3f586
Saurabh-Bhatia-Official <ssbhatia2957@gmail.com>
2026-09-07 23:20:31 +0530

    feat: configure C/C++ IntelliSense and add Linux platform include stubs for cross-platform development support

--- linux/include_stubs/flutter_linux/flutter_linux.h
+++ linux/include_stubs/flutter_linux/flutter_linux.h
@@ -1,6 +1,14 @@
 #pragma once
 
-#include <gtk/gtk.h>
+#if defined(__has_include)
+  #if __has_include("../gtk/gtk.h")
+    #include "../gtk/gtk.h"
+  #elif __has_include(<gtk/gtk.h>)
+    #include <gtk/gtk.h>
+  #endif
+#else
+  #include "../gtk/gtk.h"
+#endif
 
 #ifdef __cplusplus
 extern "C" {
--- linux/include_stubs/gdk/gdkx.h
+++ linux/include_stubs/gdk/gdkx.h
@@ -1,2 +1,10 @@
 #pragma once
-#include <gtk/gtk.h>
+#if defined(__has_include)
+  #if __has_include("../gtk/gtk.h")
+    #include "../gtk/gtk.h"
+  #elif __has_include(<gtk/gtk.h>)
+    #include <gtk/gtk.h>
+  #endif
+#else
+  #include "../gtk/gtk.h"
+#endif
